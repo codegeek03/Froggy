00e1c6114120e23322b7c28e3899de96c3cc70ce
codegeek03 <shamikbhattacharjee03@kgpian.iitkgp.ac.in>
2023-07-04 15:49:42 +0530

    m

--- script.js
+++ script.js
@@ -1,95 +1,67 @@
-
-
 // Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", function() {
+    
+// Get the necessary elements
+var btn = document.getElementById("btn");
+
+var mouseEnterCount = 0;
+
+// Add click event listener to the button
+btn.addEventListener("click", function() {
     // Get the necessary elements
+    
     var frogContainer = document.getElementById("frog-container");
     var points = document.getElementById("points");
-    var clickCount = 0;
-  
-    // Add click event listener to frogContainer
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the necessary elements
-    var frogContainer = document.getElementById("frog-container");
-    var points = document.getElementById("points");
-    var clickCount = 0;
-  
+
+    var startTime;
+    var lastX = 0;
+    var lastY = 0;
+
+    frogContainer.style.display="block";
+
     frogContainer.addEventListener("click", function() {
-      clickCount++;
-      points.textContent = "Clicks: " + clickCount;
-  
-      var currentTime = Date.now();
-      if (clickCount < 10 && currentTime < startTime + 20000) {
-        points.textContent = "You Failed";
-      } else {
-        points.textContent = "Success";
-      }
+        mouseEnterCount++;
+        points.textContent = "Your Points: " + mouseEnterCount;
+        var currentTime = Date.now();
     });
-  
-    var startTime = Date.now();
-  
-  });
-  
-  });
 
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the necessary elements
-    var frogContainer = document.getElementById("frog-container");
-  
+    function generateRandomValues() {
+        var randomX = Math.floor(Math.random() * window.innerWidth) + "px";
+        var randomY = Math.floor(Math.random() * window.innerHeight) + "px";
+        frogContainer.style.left = lastX;
+        frogContainer.style.top = lastY;
+        lastX = randomX;
+        lastY = randomY;
+    }
+
+    frogContainer.addEventListener("animationiteration", function() {
+        generateRandomValues();
+    });
+
     // Function to generate random values for animation
     function generateRandomValues() {
-      var randomX = Math.floor(Math.random() * window.innerWidth) + "px";
-      var randomY = Math.floor(Math.random() * window.innerHeight) + "px";
-      frogContainer.style.setProperty("--randomX", randomX);
-      frogContainer.style.setProperty("--randomY", randomY);
+        var randomX = Math.floor(Math.random() * window.innerWidth) + "px";
+        var randomY = Math.floor(Math.random() * window.innerHeight) + "px";
+        frogContainer.style.setProperty("--randomX", randomX);
+        frogContainer.style.setProperty("--randomY", randomY);
     }
-  
+
     // Trigger the function on animation iteration
     frogContainer.addEventListener("animationiteration", generateRandomValues);
-  
+
     // Generate random values for the initial animation
     generateRandomValues();
-  });
-  
-  
+});
 
-//   var X=[];
-//   var Y=[];
-//   function genX(){
-//       for(i=0;i<1000;i++){
-//           X.push(i)
-//       }
-//   }
-//   function genY(){
-//       for(i=0;i<600;i++){
-//           Y.push(i)
-//       }
-//   }
-//   genX();
-//   genY();
-  
-//   function getX() {
-//       var randomIndex = Math.floor(Math.random() * numbers.length);
-//       var randomNumber = X[randomIndex];
-//       return randomNumber;
-//     }
+    var frogContainer = document.getElementById("frog-container");
+    var points = document.getElementById("points");
+ 
+
+    points.textContent = "Your Points: " + mouseEnterCount + currentTime+startTime;
     
-//     var randomX = getX();
-  
-//     function getY() {
-//       var randomIndex = Math.floor(Math.random() * numbers.length);
-//       var randomNumber = Y[randomIndex];
-//       return randomNumber;
-//     }
-    
-//     var randomY = getY();
-  
+});
 
-
-
-
-
-
-
+if(mouseEnterCount<10 && currentTime==startTime+10000)
+{
+    points.textContent=points.textContent+"You failed!"
+}
